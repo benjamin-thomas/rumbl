3fd57db8667a264d4e7770ca4ee778d55c2f4670
Benjamin <benjamin.guy.thomas@gmail.com>
2022-09-03 15:26:27 +0200

    Switch to real repo

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -1,4 +1,14 @@
 defmodule Rumbl.Accounts.User do
-  @type t :: %Rumbl.Accounts.User{id: String.t(), name: String.t(), username: String.t()}
-  defstruct [:id, :name, :username]
+  # @type t :: %Rumbl.Accounts.User{id: String.t(), name: String.t(), username: String.t()}
+  # defstruct [:id, :name, :username]
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+
+    timestamps()
+  end
 end
