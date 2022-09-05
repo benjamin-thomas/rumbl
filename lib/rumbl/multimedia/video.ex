36f1698e28dff94a0243e1fcfdb59f868f3daf6a
Benjamin <benjamin.guy.thomas@gmail.com>
2022-09-05 18:15:36 +0200

    Add categories, belonging to videos

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -4,6 +4,7 @@ defmodule Rumbl.Multimedia.Video do
 
   schema "videos" do
     belongs_to :user, Rumbl.Accounts.User
+    belongs_to :category, Rumbl.Multimedia.Category
 
     field :description, :string
     field :title, :string
@@ -18,7 +19,7 @@ defmodule Rumbl.Multimedia.Video do
     # `cast` lists which fields the user may specify.
     # `validate_required` tells Ecto which fields **must be present** from the `cast` list.
     # That's why `user_id` can't be mentioned here.
-    |> cast(attrs, [:url, :title, :description])
+    |> cast(attrs, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
   end
 end
